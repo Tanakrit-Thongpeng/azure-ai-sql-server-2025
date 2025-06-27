ac494701cdfbf2699efde0ca3c751042972a3ca8
Tanakrit-Thongpeng <tanakrit.thongpeng@gmail.com>
2025-06-27 15:17:09 +0700

    Add Schema.sql query

---
+++ sql_script/Schema.sql
@@ -0,0 +1,9 @@
+-- =============================================
+-- Author:		TANAKRIT-THONGPENG
+-- Create date: 27/06/2025
+-- Description:	SCHEMA QUERY
+-- =============================================
+
+SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+ORDER BY TABLE_SCHEMA, TABLE_NAME, ORDINAL_POSITION;
